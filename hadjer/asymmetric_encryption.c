776fca50ccc72c70d89c615b6b022e3fab28d61b
Chelihi Hadjer <mingi2003yungi@gmail.com>
2024-12-21 22:47:09 +0100

    Ascii

--- hadjer/asymmetric_encryption.c
+++ hadjer/asymmetric_encryption.c
@@ -1,5 +1,24 @@
 #include<stdio.h>
+void Ascii(char text[40],int ascii[40]){
+    int i;
+    for (i=0;i<40;i++){
+        ascii[i]=(int)text[i];
+       
+    }
+
+}
+
+
 int main(){
-    printf("this is the code");
+    char text[40]="This is the text i want to encrypt";
+    int i;
+    int ascii_of_text[40];
+    Ascii(text,ascii_of_text);
+    for (i=0;i<40;i++){
+        printf("%d",ascii_of_text[i]);
+
+    }
+
+
     return 0;
 }
