2afef2a9fad0febc58aa9d554c5f67ec267e1df4
Chelihi Hadjer <mingi2003yungi@gmail.com>
2024-12-22 11:12:45 +0100

    private key

--- hadjer/asymmetric_encryption.c
+++ hadjer/asymmetric_encryption.c
@@ -25,7 +25,7 @@ int prime(int E){
     return 0;
 }
 //verifies if the public key is not a factor of the totient//
-int fact_tot(int E, intT){
+int fact_totient(int E, int T){
     if (T%E==0){
         return 0;
     }
@@ -37,24 +37,44 @@ int fact_tot(int E, intT){
 3*it mustn't be a factor of the totient
 */
 int public_key(int E,int T){
-    if (prime(E)==0 && E<=T && fact_tot(E,T)==1){
+    if (prime(E)==0 && E<=T && fact_totient(E,T)==1){
         return 0;
     }
     return 1;
 
 }
-void Encrypt(int ascii[40],int ciphertext[40]){
-    int p,q,n,T,i,E;
-    p=7;
-    q=13;
-    n=p*q;
-    scanf("%d",&E);
+int Generating_keys(int q,int p){
+    int n,T;
+    if(prime(p)==1 || prime(q)){
+        printf("the number you just antered is not prime try another one ");
+    }
+    n=q*p; //semi prime number//
     T=(p-1)*(q-1);
+    return T,n;
+}
+void Encrypt(int ascii[40],int ciphertext[40],int E,int T){
+    int i;
     if (public_key(E,T)==0){
 
     for(i=0;i<40;i++){
         ciphertext[i]=ascii[i]^E%T;
     }
+    }
+    else return 1;
+
+}
+int private_key(int E, int T,int D){
+    if ((D*E)%T==1){
+        return 0;
+    }
+    return 1;
+}
+void decrypt(int ciphertext[40],int ascii[40],int D,int n,int T,int E){
+    int i;
+    if( private_key(E,T,D)==0){
+        for(i=0;i<40;i++){
+            ascii[i]=(ciphertext[i]^D)%n;
+        }
     }
 
 
